d99fe86951ed2fd86ab49d90d97ea01be2a585cd
Paweł Dymek <melasa1998@gmail.com>
2024-06-24 00:31:15 +0200

    Bubble Sort

---
+++ AlgorithmsVisualisator/Algorithms/Sorting.cs
@@ -0,0 +1,50 @@
+﻿using System.Numerics;
+
+namespace AlgorithmsVisualisator.Algorithms
+{
+    public static class Sorting<T> where T : IComparable
+    {
+        public static void WriteArray(T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static T[] QuickSort(T[] array, bool ascending, int pivot)
+        {
+            return array;
+        }
+
+        public static T[] BubbleSort(T[] array, bool ascending) 
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    if (ascending)
+                    {
+                        if (array[i].CompareTo(array[j]) > 0)
+                        {
+                            T tmp = array[i];
+                            array[i] = array[j];
+                            array[j] = tmp;
+                        }
+                    }
+                    else
+                    {
+                        if (array[i].CompareTo(array[j]) < 0)
+                        {
+                            T tmp = array[i];
+                            array[i] = array[j];
+                            array[j] = tmp;
+                        }
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ AlgorithmsVisualisator/Program.cs
@@ -0,0 +1,40 @@
+using AlgorithmsVisualisator.Algorithms;
+
+namespace AlgorithmsVisualisator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            int[] array = { 3, 2, 1, 5, 4, 7, 6, 0, 9, 8 };
+            Console.WriteLine(array);
+            Sorting<int>.WriteArray(array);
+            var res = Sorting<int>.BubbleSort(array, true);
+            Sorting<int>.WriteArray(res);
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
